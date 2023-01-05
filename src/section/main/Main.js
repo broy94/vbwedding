836867987ae48c59a295c12e7dbee61ebe3a45d3
Biswajit Roy <brr@qad.com>
2023-01-05 18:30:51 +0100

    use flex in Main page

--- src/section/main/Main.js
+++ src/section/main/Main.js
@@ -1,7 +1,5 @@
 import "./Main.css";
 import Section from "../Section";
-import Container from "react-bootstrap/Container";
-import {Col, Row} from "react-bootstrap";
 import {localizedStrings} from "../../Localization";
 import RsvpButton from "../../rsvp-button/RsvpButton";
 
@@ -10,33 +8,23 @@ const googleMapsLink = "https://goo.gl/maps/c9ff9wVG3VXiVAAV8";
 function Main() {
     return (
         <Section id="main">
-            <Container className="pt-3">
-                <Row>
-                    <Col className="my-4">
-                        <h4>{localizedStrings.main.date}</h4>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="mt-3 mb-2">
-                        <h1 className="display-1 title">{localizedStrings.main.title}</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="my-1">
-                        <a href={googleMapsLink} target="_blank" rel="noreferrer">
-                            <span className="button hover-button py-2">
-                                <h4 className="mb-0">{localizedStrings.main.venue.title}</h4>
-                                <small>{localizedStrings.main.venue.location}</small>
-                            </span>
-                        </a>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <RsvpButton />
-                    </Col>
-                </Row>
-            </Container>
+            <div className="d-flex justify-content-center py-4">
+                <h4>{localizedStrings.main.date}</h4>
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+                <h1 className="display-1 title">{localizedStrings.main.title}</h1>
+            </div>
+            <div className="d-flex justify-content-center my-1">
+                <a href={googleMapsLink} target="_blank" rel="noreferrer">
+                    <span className="button hover-button py-2">
+                        <h4 className="mb-0">{localizedStrings.main.venue.title}</h4>
+                        <small>{localizedStrings.main.venue.location}</small>
+                    </span>
+                </a>
+            </div>
+            <div className="d-flex justify-content-center">
+                <RsvpButton/>
+            </div>
         </Section>
     );
 }
